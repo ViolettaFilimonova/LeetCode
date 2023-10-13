4f4d3e1dcdc51ddc6f456f46add3d6a43fb29cf9
Violetta99 <violetta99.filimonova@mail.ru>
2023-10-13 15:30:45 +0300

    'add: twoSum( [3,2,3], 6)'

--- twoSum.js
+++ twoSum.js
@@ -15,7 +15,7 @@
 const twoSum = (nums, target) => {
     const indexSum = []
     for (let i = 0; i < nums.length; i++) {
-        for (let j = 1; j < nums.length; j++) {
+        for (let j = i+ 1; j < nums.length; j++) {
             if(nums[i] + nums[j] === target){
             indexSum.push(i,j)
             nums.splice(i,1)
@@ -27,5 +27,6 @@ const twoSum = (nums, target) => {
 }
 console.log(twoSum( [2,7,11,15], 9));
 console.log(twoSum( [3,2,4], 6));
+console.log(twoSum( [3,2,3], 6));
 console.log(twoSum( [3,3], 6));
 
